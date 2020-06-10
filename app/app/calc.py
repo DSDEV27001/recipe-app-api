bcda30835d6d537698eee0209e92e62234bfffbb
DSDEV27001 <52406362+DSDEV27001@users.noreply.github.com>
2020-06-10 16:51:10 +0100

    Created core app

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    """subtract x from y and return value"""
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from app.calc import add
+from app.calc import subtract
+
+
+class CalcTests(TestCase):
+    def test_add_numbers(self):
+        """test two numbers are added together"""
+        self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        """test that values are subhtracted and returned"""
+        self.assertEqual(subtract(5, 11), 6)
