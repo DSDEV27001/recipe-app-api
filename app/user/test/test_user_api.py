760a86b77f251e4ecbcf14b31f3ce2922f35bae3
DSDEV27001 <52406362+DSDEV27001@users.noreply.github.com>
2020-06-15 17:37:46 +0100

    Added endpoint for creating and authenticating users

--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -8,7 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse("user:create")
 TOKEN_URL = reverse("user:token")
-
+ME_URL = reverse("user:me")
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -108,3 +108,47 @@ class PublicUserApiTests(TestCase):
 
         self.assertNotIn("token", res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is requried for users"""
+
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Tests API requests that require authentication"""
+
+    def setUp(self):
+        self.uesr = create_user(
+            email="test@londonappdev.com",
+            password="testpass",
+            name="name"
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code,status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            "name": self.user.name,
+            "email": self.user.email,
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me URL"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for an authenticated user"""
+        payload = {"name": "new name", "password": "newpassword123"}
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload["name"])
+        self.assertTrue(res.status_code, status.HTTP_200_OK)
