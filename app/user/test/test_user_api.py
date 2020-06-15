6666bab1d2ef3cd6281d8105a7be6b21b7ab8e15
DSDEV27001 <52406362+DSDEV27001@users.noreply.github.com>
2020-06-15 14:05:36 +0100

    Added "name" to payload in tests

--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -35,7 +35,7 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating a user that already exists fails"""
-        payload = {"email": "test@londonappdev.com", "password": "testpass"}
+        payload = {"email": "test@londonappdev.com", "password": "testpass", "name": "Test"}
         create_user(**payload)
 
         res = self.client.post(CREATE_USER_URL, payload)
@@ -44,7 +44,7 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that the password must be more than 5 characters"""
-        payload = {"email": "test@londonappdev.com", "password": "pw"}
+        payload = {"email": "test@londonappdev.com", "password": "pw", "name": "Test"}
 
         res = self.client.post(CREATE_USER_URL, payload)
 
